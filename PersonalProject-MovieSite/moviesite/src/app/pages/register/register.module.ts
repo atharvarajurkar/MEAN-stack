39e4f27b4f2aa8260369e02ae975f0c63e0aad50
Atharva Rajurkar <asrajurk@cougarnet.uh.edu>
2024-06-29 15:00:04 -0500

    moviesite update

--- PersonalProject-MovieSite/moviesite/src/app/pages/register/register.module.ts
+++ PersonalProject-MovieSite/moviesite/src/app/pages/register/register.module.ts
@@ -5,6 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { SharedModule } from '../../shared/shared/shared.module';
 import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { Step3Component } from './step3/step3.component';
 
 const routes: Routes = [
   {
@@ -12,6 +19,7 @@ const routes: Routes = [
     component: RegisterComponent,
     children: [
       { path: 'step1', component: Step1Component },
+      { path: 'step2', component: Step2Component },
       { path: '', redirectTo: 'step1', pathMatch: 'full' },
     ],
   },
@@ -20,11 +28,19 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     RegisterComponent,
-    Step1Component
+    Step1Component,
+    Step2Component,
+    Step3Component
   ],
   imports: [
     CommonModule,
     SharedModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatInputModule,
     RouterModule.forChild(routes)
   ]
 })
